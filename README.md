d13edecc07052588c790c1f131da43cd79d357b6
prostiak2019 <yarovenkodmv@gmail.com>
2020-11-04 21:32:11 +0300

    init: start map-task

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Team_task_map
+
+1) Yarovenko-Dmitry 
+2) NetFound
+3) alex170485
+4) KhazanAlexey
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>1) Yarovenko-Dmitry</div>
+      <div>2) NetFound</div>
+      <div>3) alex170485</div>
+      <div>4) KhazanAlexey</div>
+    </div>
+  );
+}
+
+export default App;
