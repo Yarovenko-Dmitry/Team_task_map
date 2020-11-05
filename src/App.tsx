617b3c4bb4b1974169b12ed0127e3562dcae334d
Алекс <alex.170485@icloud.com>
2020-11-05 22:10:30 +0300

    feat: add new branch alex170485

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <div>1) Yarovenko-Dmitry</div>
-      <div>2) NetFound</div>
-      <div>3) alex170485</div>
-      <div>4) KhazanAlexey</div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div>1) alex170485</div>
+            <div>2) Yarovenko-Dmitry</div>
+            <div>3) NetFound</div>
+            <div>4) KhazanAlexey</div>
+            <div>5) Belik85</div>
+        </div>
+            );
 }
 
 export default App;
