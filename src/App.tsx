1172c48b6eece42c3bd044fb7dd168ba4cd2fc54
Belik85 <beletskyidmytro@gmail.com>
2020-11-05 20:29:48 +0100

    fiat: add new branch

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <div>1) Yarovenko-Dmitry</div>
-      <div>2) NetFound</div>
-      <div>3) alex170485</div>
-      <div>4) KhazanAlexey</div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div>1) Belik85</div>
+            <div>2) Yarovenko-Dmitry</div>
+            <div>3) NetFound</div>
+            <div>4) alex170485</div>
+            <div>5) KhazanAlexey</div>
+        </div>
+    );
 }
 
 export default App;
