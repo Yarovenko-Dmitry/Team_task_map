6e5307d5c3b39c0ad2507c471e927097d5c68478
khaza <>
2020-11-05 22:28:37 +0300

    fiat: add ne branch Khazanalexey

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div>1) Yarovenko-Dmitry</div>
+      <div>1) KhazanAlexey</div>
       <div>2) NetFound</div>
       <div>3) alex170485</div>
-      <div>4) KhazanAlexey</div>
+      <div>4)Yarovenko-Dmitry </div>
     </div>
   );
 }
