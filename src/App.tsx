3e481f383cae418b19f41e3d5af24d8bb6a7efca
prostiak2019 <yarovenkodmv@gmail.com>
2020-11-21 16:09:12 +0300

    feat: find & add objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React, {ChangeEvent, useState} from 'react';
 import './App.css';
 import {Map, RulerControl, TypeSelector, YMaps, ZoomControl, Placemark} from 'react-yandex-maps';
-import {onClickSearchObjectsHandler} from './hendlers/apiHendler';
+import {onClickSearchObjectsHandler, onClickShowNearbyObjectsHeddler} from './hendlers/apiHendler';
 import {v1} from 'uuid';
-
+import pointer from '../src/assets/cut-map-marker-final.jpg'
 
 type ItMinskSchoolType = {
   schoolDescription: string,
@@ -13,9 +13,19 @@ type ItMinskSchoolType = {
   newSchoolLongitude: any
 }
 
+type displaySearchObjectType = {
+  description: string,
+  id: string,
+  schoolName: string,
+  newSchoolLatitude: any,
+  newSchoolLongitude: any
+  phone: any
+}
+
 function App() {
 
   const TESTitMinskSchools: Array<ItMinskSchoolType> = [];
+  const StartDisplaySearchObjects: Array<ItMinskSchoolType> = [];
 
   const [schoolName, setSchoolName] = useState<string>('');
   const [schoolDescription, setSchoolDescription] = useState<string>('');
@@ -23,7 +33,13 @@ function App() {
   const [newSchoolLongitude, setNewSchoolLongitude] = useState<any>(0);
   const [itMinskSchools, setItMinskSchools] = useState<Array<ItMinskSchoolType>>(TESTitMinskSchools);
 
-  const [locationSearchObject, setLocationSearchObject] = useState<string>('');
+  const [searchObjectLocation, setSearchObjectLocation] = useState<string>('');
+  const [searchObjectType, setSearchObjectType] = useState<string>('');
+  const [streetName, setStreetName] = useState<string>('');
+  const [searchObjectCount, setSearchObjectCount] = useState<number>(1);
+  const [displaySearchObjects, setDisplaySearchObjects] = useState<Array<any>>(StartDisplaySearchObjects);
+
+
   const [searchObjectLatitude, setSearchObjectLatitude] = useState<number>(50.5000);
   const [searchObjectLongitude, setSearchObjectLongitude] = useState<number>(30.5000);
 
@@ -50,7 +66,6 @@ function App() {
 
   const onClickAddSchoolButtonHandler = () => {
     addNewItSchoolMinsk(schoolDescription, schoolName);
-    // console.log(itMinskSchools)
     setSearchObjectLatitude(newSchoolLatitude);
     setSearchObjectLongitude(newSchoolLongitude);
   }
@@ -67,11 +82,11 @@ function App() {
   }
 
   const onChangeLocationNameHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    setLocationSearchObject(e.currentTarget.value);
+    setSearchObjectLocation(e.currentTarget.value);
   }
 
-  const onClickSearchObjectsButtonHeddler = () => {
-    onClickSearchObjectsHandler(locationSearchObject).then((coordinates: Array<string>) => {
+  const onClickSearchLocationButtonHeddler = () => {
+    onClickSearchObjectsHandler(searchObjectLocation).then((coordinates: Array<string>) => {
       if (coordinates.length) {
         setSearchObjectLatitude(Number(coordinates[0]));
         setSearchObjectLongitude(+coordinates[1]);
@@ -79,73 +94,85 @@ function App() {
     })
   }
 
+  const onChangeSearchObjectTypeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchObjectType(e.currentTarget.value);
+  }
+
+  const onChangeSearchObjectCountHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchObjectCount(Math.round(+e.currentTarget.value));
+  }
+
+  const onChangeStreetNameHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setStreetName(e.currentTarget.value);
+  }
+
+  const onClickShowNearbyObjectsButtonHeddler = () => {
+    onClickShowNearbyObjectsHeddler(searchObjectType, searchObjectLocation, streetName, searchObjectCount ).then((foundObjects: any[]) => {
+      if (foundObjects.length) {
+        let shownObjects: Array<displaySearchObjectType> = new Array();
+        for (let i=0; i< foundObjects.length; i++){
+          shownObjects[i] = {
+            description : foundObjects[i].properties.description ,
+            id: v1(),
+            schoolName: foundObjects[i].properties.name,
+            newSchoolLatitude: foundObjects[i].geometry.coordinates[1],
+            newSchoolLongitude: foundObjects[i].geometry.coordinates[0],
+            phone: 777654
+          }
+        }
+        setDisplaySearchObjects(shownObjects);
+      }
+    })
+  }
+
   const getMapCoordinates = (e: any) => {
-    // console.log(e)
-    let coordinatesSchool = e.get('coords')
-    // console.log(coordinatesSchool)
+    let coordinatesSchool = e.get('coords');
     setNewSchoolLatitude(+coordinatesSchool[0]);
     setNewSchoolLongitude(+coordinatesSchool[1]);
-
-    // setSearchObjectLatitude(+coordinatesSchool[0]);
-    // setSearchObjectLongitude(+coordinatesSchool[1]);
   }
 
   return (
     <div className="App">
       <div className={'searchAddNavigation'}>
-        {/* потом может зарефакторим в формик*/}
         <div className={'addObject'}>
-          <div>добавить школу</div>
+          <div>Добавить школу</div>
           <div>
-            {/*<div>название школы</div>*/}
             <input type={'text'} name={'schoolName'} value={schoolName} placeholder={'Название школы'}
                    onChange={onChangeSchoolNameHandler}/>
           </div>
           <div>
-            {/*<div>описание</div>*/}
-            <input type='text' placeholder={'Описание'} onChange={onChangeDescriptionHandler}/>
+            <input type={'text'} name={'schoolDescription'} value={schoolDescription} placeholder={'Описание'}
+                   onChange={onChangeDescriptionHandler}/>
           </div>
           <div>
-            <div>координаты</div>
-            <input value={newSchoolLatitude} type={'text'} placeholder={'Широта'}
+            <div>Координаты</div>
+            <input type={'text'} value={newSchoolLatitude} placeholder={'Широта'}
                    onChange={onChangeNewSchoolLatitudeHandler}/>
-
-            <input value={newSchoolLongitude} type={'text'} placeholder={'Долгота'}
+            <input type={'text'} value={newSchoolLongitude} placeholder={'Долгота'}
                    onChange={onChangeNewSchoolLongitudeHandler}/>
           </div>
-          {/*<input type={'button'} name={'selectCoordinate'} value={'Указать объект на карте'}/>*/}
-          {/*  <div>*/}
-          {/*  /!* возможно добавить изображение с видом метки*!/*/}
-          {/*  <input type={'button'} name={'changeMarkType'} value={'Сменить вид метки'}/>*/}
-          {/*  <input type={'text'} name={'markType'} placeholder={'Mark type'}/>*/}
-          {/*</div>*/}
           <input type={'button'} name={'addNewItSchoolMinsk'} value={'Добавить на карту'}
                  onClick={onClickAddSchoolButtonHandler}/>
         </div>
         <div className={'searchObject'}>
-          <div>Найти школы</div>
+          <div>Найти объект(ы)</div>
           <div>
-            {/*<div>Местность</div>*/}
-            <input type={'text'} name={'location'} placeholder={'Название объекта'}
+            <input type={'text'} name={'locationName'} value={searchObjectLocation} placeholder={'Локация'}
                    onChange={onChangeLocationNameHandler}/>
-            <input type={'button'} name={'searchObjects'} value={'Найти объект'}
-                   onClick={onClickSearchObjectsButtonHeddler}/>
-            {/*<div>*/}
-            {/*  <div>координаты</div>*/}
-            {/*  <input type={'text'} name={'latitudeCoordinate'} placeholder={'Latitude'}/>*/}
-            {/*  <input type={'text'} name={'longitudeCoordinate'} placeholder={'Longitude'}/>*/}
-            {/*</div>*/}
+            <input type={'button'} name={'searchLocation'} value={'Показать локацию на карте'}
+                   onClick={onClickSearchLocationButtonHeddler}/>
           </div>
-          {/*<select>*/}
-          {/*  <option>Школа для детей</option>*/}
-          {/*  <option>Курсы для взрослых</option>*/}
-          {/*  <option>IT ВУЗ</option>*/}
-          {/*</select>*/}
           <div>
-            <div>Количество ближайших школ (1-5)</div>
-            <input type={'number'} name={'objectNumber'} min="1" max="5"/>
+            <div>Уточнение поиска</div>
+            <input type={'text'} name={'searchObjectType'} value={searchObjectType} placeholder={'Тип объекта'}
+                   onChange={onChangeSearchObjectTypeHandler}/>
+            <input type={'text'} value={streetName} name={'streatName'} placeholder={'Название улицы'}
+                   onChange={onChangeStreetNameHandler}/>
+            <input type={'number'} name={'objectCount'} value={searchObjectCount} min="1" max="20"
+                   placeholder={"мах кол-во"} onChange={onChangeSearchObjectCountHandler}/>
           </div>
-          <input type={'button'} name={'showNearbySchools'} value={'Показать на карте'}/>
+          <input type={'button'} name={'showNearbyObjects'} value={'Показать объект(ы) на карте'}
+                 onClick={onClickShowNearbyObjectsButtonHeddler}/>
         </div>
       </div>
       <div className={'mapArea'}>
@@ -159,7 +186,21 @@ function App() {
               <ZoomControl options={{position: {right: 10, top: 10}}}/>
               <TypeSelector options={{position: {left: 10, top: 10}}}/>
               <RulerControl options={{position: {right: 50, top: 10}}}/>
-
+              {displaySearchObjects.map((displaySearchObject: displaySearchObjectType) => <Placemark
+                  geometry={[displaySearchObject.newSchoolLatitude, displaySearchObject.newSchoolLongitude]}
+                  modules={['geoObject.addon.balloon', 'geoObject.addon.hint']}
+                  properties={{
+                    hintContent: displaySearchObject.schoolName,
+                    balloonContent: displaySearchObject.description,
+                  }}
+                  options={{
+                    iconLayout: 'default#image',
+                    iconImageHref: pointer,
+                    iconImageSize: [40, 50],
+                    iconImageOffset: [-20, -55],
+                  }}
+                />
+              )}
               {itMinskSchools.map((itMinskSchool: ItMinskSchoolType) => <Placemark
                   geometry={[itMinskSchool.newSchoolLatitude, itMinskSchool.newSchoolLongitude]}
                   modules={['geoObject.addon.balloon', 'geoObject.addon.hint']}
--- src/hendlers/apiHendler.ts
+++ src/hendlers/apiHendler.ts
@@ -1,14 +1,22 @@
 import {mainRequest} from '../api/api';
 
-
 export const onClickSearchObjectsHandler = (location: string) => {
   return mainRequest.getObjectCoordinates(location)
-    .then(res=>{
-      let pos = res.data.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos
-       let coordinats = pos.split(' ').reverse()
-  return coordinats
-    }
-      ).catch(err => {
+    .then(res => {
+        const pos = res.data.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos
+        const coordinats = pos.split(' ').reverse()
+        return coordinats
+      }
+    ).catch(err => {
+    })
+}
 
-  })
+export const onClickShowNearbyObjectsHeddler = (objectType: string, location: string, streetName: string, objectCount: number) => {
+  return mainRequest.getDesplayObjects(objectType, location, streetName, objectCount)
+    .then(res => {
+        const features = res.data.features
+        return features
+      }
+    ).catch(err => {
+    })
 }
