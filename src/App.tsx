ce870540fed8d361d3fe22258f4694381476084a
NetFound <prosto5564821@gmail.com>
2020-11-22 20:36:35 +0300

    fix: merge commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,205 +1,211 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, useCallback, useState} from 'react';
 import './App.css';
-import {Map, RulerControl, TypeSelector, YMaps, ZoomControl, Placemark} from 'react-yandex-maps';
-import {onClickSearchObjectsHandler} from './hendlers/apiHendler';
+import {onClickSearchObjectsHandler, onClickShowNearbyObjectsHeddler} from './hendlers/apiHendler';
 import {v1} from 'uuid';
+import {MyMappTEST} from './Components/MyMappTEST';
+import {Switch} from '@material-ui/core';
 
 
-type ItMinskSchoolType = {
-    schoolDescription: string,
-    id: string,
-    schoolName: string,
-    newSchoolLatitude: any,
-    newSchoolLongitude: any
+export type ItMinskSchoolType = {
+  schoolDescription: string,
+  id: string,
+  schoolName: string,
+  newSchoolLatitude: number,
+  newSchoolLongitude: number
+}
+
+export type displaySearchObjectType = {
+  description: string,
+  id: string,
+  schoolName: string,
+  newSchoolLatitude: number,
+  newSchoolLongitude: number
+  phone: any
 }
 
 function App() {
 
-    const TESTitMinskSchools = [
-        {
-            id: 'd2c832a0-28d6-11eb-a6bf-0d5927730644',
-            newSchoolLatitude: 50.4292,
-            newSchoolLongitude: 30.4931,
-            schoolDescription: 'тест 1',
-            schoolName: '1'
-        },
-        {
-            id: 'd2c832a0-28d6-11eb-a6bf-0d5927730645',
-            newSchoolLatitude: 50.4590,
-            newSchoolLongitude: 30.5391,
-            schoolDescription: 'тест 2',
-            schoolName: '2'
-        },
-        {
-            id: 'd2c832a0-28d6-11eb-a6bf-0d5927730646',
-            newSchoolLatitude: 50.4149,
-            newSchoolLongitude: 30.6057,
-            schoolDescription: 'тест 3',
-            schoolName: '3'
-        }
-    ]
+  const TESTitMinskSchools: Array<ItMinskSchoolType> = [];
+  const StartDisplaySearchObjects: Array<ItMinskSchoolType> = [];
 
-    const [schoolName, setSchoolName] = useState<string>('');
-    const [schoolDescription, setSchoolDescription] = useState<string>('');
-    const [newSchoolLatitude, setNewSchoolLatitude] = useState<any>(0);
-    const [newSchoolLongitude, setNewSchoolLongitude] = useState<any>(0);
-    const [itMinskSchools, setItMinskSchools] = useState<Array<ItMinskSchoolType>>(TESTitMinskSchools);
+  const [schoolName, setSchoolName] = useState<string>('');
+  const [schoolDescription, setSchoolDescription] = useState<string>('');
+  const [newSchoolLatitude, setNewSchoolLatitude] = useState<any>(0);
+  const [newSchoolLongitude, setNewSchoolLongitude] = useState<any>(0);
+  const [itMinskSchools, setItMinskSchools] = useState<Array<ItMinskSchoolType>>(TESTitMinskSchools);
 
-    const [locationSearchObject, setLocationSearchObject] = useState<string>('');
-    const [searchObjectLatitude, setSearchObjectLatitude] = useState<number>(50.5000);
-    const [searchObjectLongitude, setSearchObjectLongitude] = useState<number>(30.5000);
+  const [searchObjectLocation, setSearchObjectLocation] = useState<string>('');
+  const [searchObjectType, setSearchObjectType] = useState<string>('');
+  const [streetName, setStreetName] = useState<string>('');
+  const [searchObjectCount, setSearchObjectCount] = useState<number>(1);
+  const [displaySearchObjects, setDisplaySearchObjects] = useState<Array<any>>(StartDisplaySearchObjects);
 
-    const onChangeSchoolNameHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        setSchoolName(e.currentTarget.value);
+
+  const [searchObjectLatitude, setSearchObjectLatitude] = useState<number>(50.5000);
+  const [searchObjectLongitude, setSearchObjectLongitude] = useState<number>(30.5000);
+
+  const [showingAddObjectNavigation, setShowingAddObjectNavigation] = useState<boolean>(true);
+
+  const changeToggle = (e: any) => {
+    setShowingAddObjectNavigation(e.target.checked)
+    console.log('showingAddObjectNavigation ', showingAddObjectNavigation)
+  };
+
+
+  const onChangeSchoolNameHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setSchoolName(e.currentTarget.value);
+  }
+
+  const onChangeDescriptionHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.currentTarget.value.length > 20) {
+      console.log('Value is more')
     }
+    setSchoolDescription(e.currentTarget.value)
+  }
 
-    const onChangeDescriptionHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        if (e.currentTarget.value.length > 20) {
-            console.log('Value is more')
-        }
-        setSchoolDescription(e.currentTarget.value)
-    }
+  const onChangeNewSchoolLatitudeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const reg = /[^\d\.]/g
+    setNewSchoolLatitude(e.currentTarget.value.replace(reg, ''))
+  }
 
-    const onChangeNewSchoolLatitudeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        const reg = /[^\d\.]/g
-        setNewSchoolLatitude(e.currentTarget.value.replace(reg, ''))
-    }
+  const onChangeNewSchoolLongitudeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const reg = /[^\d\.]/g
+    setNewSchoolLongitude(e.currentTarget.value.replace(reg, ''))
+  }
 
-    const onChangeNewSchoolLongitudeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        const reg = /[^\d\.]/g
-        setNewSchoolLongitude(e.currentTarget.value.replace(reg, ''))
-    }
+  const onClickAddSchoolButtonHandler = () => {
+    addNewItSchoolMinsk(schoolDescription, schoolName);
+    setSearchObjectLatitude(newSchoolLatitude);
+    setSearchObjectLongitude(newSchoolLongitude);
+  }
 
-    const onClickAddSchoolButtonHandler = () => {
-        addNewItSchoolMinsk(schoolDescription, schoolName)
-        console.log(itMinskSchools)
-    }
+  const addNewItSchoolMinsk = (schoolDescription: string, schoolName: string) => {
+    const newItSchoolMinsk = {
+      id: v1(),
+      schoolName,
+      schoolDescription,
+      newSchoolLatitude,
+      newSchoolLongitude
+    };
+    setItMinskSchools([...itMinskSchools, newItSchoolMinsk]);
+  }
 
-    // const addCoordinatesNewSchool = (e: any) => {
-    //     const reg = /[^\d\.]/g
-    //     setCoordinate(e.currentTarget.value.replace(reg, ''))
+  const onChangeLocationNameHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchObjectLocation(e.currentTarget.value);
+  }
 
+  const onClickSearchLocationButtonHeddler = () => {
+    onClickSearchObjectsHandler(searchObjectLocation).then((coordinates: Array<string>) => {
+      if (coordinates.length) {
+        setSearchObjectLatitude(Number(coordinates[0]));
+        setSearchObjectLongitude(+coordinates[1]);
+      }
+    })
+  }
 
-    const addNewItSchoolMinsk = (schoolDescription: string, schoolName: string) => {
-        const newItSchoolMinsk = {
+  const onChangeSearchObjectTypeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchObjectType(e.currentTarget.value);
+  }
+
+  const onChangeSearchObjectCountHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchObjectCount(Math.round(+e.currentTarget.value));
+  }
+
+  const onChangeStreetNameHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setStreetName(e.currentTarget.value);
+  }
+
+  const onClickShowNearbyObjectsButtonHeddler = () => {
+    onClickShowNearbyObjectsHeddler(searchObjectType, searchObjectLocation, streetName, searchObjectCount).then((foundObjects: any[]) => {
+      if (foundObjects.length) {
+        const shownObjects: Array<displaySearchObjectType> = foundObjects.map((obj: any) => {
+          return {
+            description: obj.properties.description,
             id: v1(),
-            schoolName,
-            schoolDescription,
-            newSchoolLatitude,
-            newSchoolLongitude
-        };
-        setItMinskSchools([...itMinskSchools, newItSchoolMinsk]);
-    }
-
-    const onChangeLocationNameHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        setLocationSearchObject(e.currentTarget.value);
-    }
-
-    const onClickSearchObjectsButtonHeddler = () => {
-        onClickSearchObjectsHandler(locationSearchObject).then((coordinates: Array<string>) => {
-            if (coordinates.length) {
-                setSearchObjectLatitude(Number(coordinates[0]));
-                setSearchObjectLongitude(+coordinates[1]);
-            }
+            schoolName: obj.properties.name,
+            newSchoolLatitude: obj.geometry.coordinates[1],
+            newSchoolLongitude: obj.geometry.coordinates[0],
+            phone: 777654
+          }
         })
-    }
+        setDisplaySearchObjects(shownObjects);
+      }
+    })
+  }
 
-    const getMapCoordinates = (e: any) => {
-        console.log(e)
-        let coordinatesSchool = e.get('coords')
-        console.log(coordinatesSchool)
-        setNewSchoolLatitude(+coordinatesSchool[0])
-        setNewSchoolLongitude(+coordinatesSchool[1])
-    }
+  const getMapCoordinates = useCallback((e: any) => {
+    let coordinatesSchool = e.get('coords');
+    setNewSchoolLatitude(+coordinatesSchool[0]);
+    setNewSchoolLongitude(+coordinatesSchool[1]);
+  }, [])
 
-    return (
-        <div className="App">
-            <div className={'searchAddNavigation'}>
-                {/* потом может зарефакторим в формик*/}
-                <div className={'addObject'}>
-                    <div>добавить школу</div>
-                    <div>
-                        <div>название школы</div>
-                        <input type={'text'} name={'schoolName'} value={schoolName}
-                               onChange={onChangeSchoolNameHandler}/>
-                    </div>
-                    <div>
-                        <div>описание</div>
-                        <input type='text' onChange={onChangeDescriptionHandler}/>
-                    </div>
-                    <div>
-                        <div>координаты</div>
-                        <input value={newSchoolLatitude} type={'text'} onChange={onChangeNewSchoolLatitudeHandler}/>
 
-                        <input value={newSchoolLongitude} type={'text'} onChange={onChangeNewSchoolLongitudeHandler}/>
-                    </div>
-                    {/*<input type={'button'} name={'selectCoordinate'} value={'Указать объект на карте'}/>*/}
-                    <div>
-                        {/* возможно добавить изображение с видом метки*/}
-                        <input type={'button'} name={'changeMarkType'} value={'Сменить вид метки'}/>
-                        <input type={'text'} name={'markType'} placeholder={'Mark type'}/>
-                    </div>
-                    <input type={'button'} name={'addNewItSchoolMinsk'} value={'Добавить на карту'}
-                           onClick={onClickAddSchoolButtonHandler}/>
-                </div>
-                <div className={'searchObject'}>
-                    <div>Найти объекты</div>
-                    <div>
-                        <div>Местность</div>
-                        <input type={'text'} name={'location'} placeholder={'Название региона'}
-                               onChange={onChangeLocationNameHandler}/>
-                        <div>
-                            <div>координаты</div>
-                            <input type={'text'} name={'latitudeCoordinate'} placeholder={'Latitude'}/>
-                            <input type={'text'} name={'longitudeCoordinate'} placeholder={'Longitude'}/>
-                        </div>
-                    </div>
-                    <select>
-                        <option>Школа для детей</option>
-                        <option>Курсы для взрослых</option>
-                        <option>IT ВУЗ</option>
-                    </select>
-                    <div>
-                        <div>Количество объектов</div>
-                        <input type={'number'} name={'objectNumber'}/>
-                    </div>
-                    <input type={'button'} name={'searchObjects'} value={'Найти объекты'}
-                           onClick={onClickSearchObjectsButtonHeddler}/>
-                </div>
-            </div>
-            <div className={'mapArea'}>
-                <YMaps>
-                    <div>
-                        <Map className={'map'}
-                             onClick={(e: any) => {
-                                 getMapCoordinates(e)
-                             }}
-                             state={{center: [searchObjectLatitude, searchObjectLongitude], zoom: 10}}>
-                            <ZoomControl options={{position: {right: 10, top: 10}}}/>
-                            <TypeSelector options={{position: {left: 10, top: 10}}}/>
-                            <RulerControl options={{position: {right: 50, top: 10}}}/>
-
-                            {itMinskSchools.map((itMinskSchool: ItMinskSchoolType) => <Placemark
-                                    geometry={[itMinskSchool.newSchoolLatitude, itMinskSchool.newSchoolLongitude]}
-                                    modules={['geoObject.addon.balloon', 'geoObject.addon.hint']}
-                                    properties={{
-                                        hintContent: itMinskSchool.schoolName,
-                                        balloonContent: itMinskSchool.schoolDescription,
-                                    }}
-                                    options={{
-                                        iconLayout: 'default#image',
-                                        iconImageHref: 'https://image.freepik.com/free-vector/3d-gps-red-color-icon-dropping-on-street-map-on-white_175838-446.jpg',
-                                        iconImageSize: [80, 60],
-                                        iconImageOffset: [0, 0],
-                                    }}
-                                />
-                            )}
-                        </Map>
-                    </div>
-                </YMaps>
-            </div>
+  return (
+    <div className="App">
+      <div className={'navigation'}>
+        <div>
+          <p>сменить режим</p>
+          <Switch
+            checked={showingAddObjectNavigation}
+            onChange={changeToggle}
+            name="toggle"
+          />
         </div>
-    );
+        {showingAddObjectNavigation
+          ? <div className={'addObjectPanel'}>
+            <div>Добавить школу</div>
+            <div>
+              <input type={'text'} name={'schoolName'} value={schoolName} placeholder={'Название школы'}
+                     onChange={onChangeSchoolNameHandler}/>
+            </div>
+            <div>
+              <input type={'text'} name={'schoolDescription'} value={schoolDescription} placeholder={'Описание'}
+                     onChange={onChangeDescriptionHandler}/>
+            </div>
+            <div>
+              <div>Координаты</div>
+              <input type={'text'} value={newSchoolLatitude} placeholder={'Широта'}
+                     onChange={onChangeNewSchoolLatitudeHandler}/>
+              <input type={'text'} value={newSchoolLongitude} placeholder={'Долгота'}
+                     onChange={onChangeNewSchoolLongitudeHandler}/>
+            </div>
+            <input type={'button'} name={'addNewItSchoolMinsk'} value={'Добавить на карту'}
+                   onClick={onClickAddSchoolButtonHandler}/>
+          </div>
+          : <div className={'searchObjectPanel'}>
+            <div>Найти объект(ы)</div>
+            <div>
+              <input type={'text'} name={'locationName'} value={searchObjectLocation} placeholder={'Локация'}
+                     onChange={onChangeLocationNameHandler}/>
+              <input type={'button'} name={'searchLocation'} value={'Показать локацию на карте'}
+                     onClick={onClickSearchLocationButtonHeddler}/>
+            </div>
+            <div>
+              <div>Уточнение поиска</div>
+              <input type={'text'} name={'searchObjectType'} value={searchObjectType} placeholder={'Тип объекта'}
+                     onChange={onChangeSearchObjectTypeHandler}/>
+              <input type={'text'} value={streetName} name={'streatName'} placeholder={'Название улицы'}
+                     onChange={onChangeStreetNameHandler}/>
+              <input type={'number'} name={'objectCount'} value={searchObjectCount} min="1" max="20"
+                     title={'Сколько максимально вывести объектов от 1 до 20'}
+                     placeholder={"мах кол-во"} onChange={onChangeSearchObjectCountHandler}/>
+            </div>
+            <input type={'button'} name={'showNearbyObjects'} value={'Показать объект(ы) на карте'}
+                   onClick={onClickShowNearbyObjectsButtonHeddler}/>
+            <p>Объектов найдено : </p> {displaySearchObjects.length}
+          </div>}
+      </div>
+      <div className={'mapArea'}>
+        <MyMappTEST getMapCoordinates={getMapCoordinates}
+                    searchObjectLatitude={searchObjectLatitude}
+                    searchObjectLongitude={searchObjectLongitude}
+                    displaySearchObjects={displaySearchObjects}
+                    itMinskSchools={itMinskSchools}/>
+      </div>
+    </div>
+  );
 }
 
 export default App;
+
+
